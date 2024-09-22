76fa835174f65acda0b104c97b42cc47b75bbc37
Ｚ１Ｎ３Ｘ  <sujoinnnaaa@gmail.com>
2024-09-22 18:14:42 +0600

    Update ig5.js

--- scripts/commands/ig5.js
+++ scripts/commands/ig5.js
@@ -19,7 +19,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["♻️--『🅰🅻🆅🅸 🅱🅾🆃』--♻️"
+   var hi = ["♻️--『SARA-ROBOT』--♻️"
  ];
   var know = hi[Math.floor(Math.random() * hi.length)];
     var link = [
