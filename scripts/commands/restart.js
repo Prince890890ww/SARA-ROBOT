1358a8d75b9e5a079627ad2146f6e7900f7c62f4
Ｚ１Ｎ３Ｘ  <sujoinnnaaa@gmail.com>
2024-09-22 18:01:16 +0600

    Update restart.js

--- scripts/commands/restart.js
+++ scripts/commands/restart.js
@@ -14,21 +14,17 @@ module.exports.config = {
 module.exports.run = async ({ api, event, args }) => {
   const { threadID, messageID } = event;
   return api.sendMessage(`╭──────────────➣
-│𝗠𝗥.𝗔𝗟𝗩𝗜 𝗦𝗘𝗥𝗩𝗘𝗥 is
+│SARA-ROBOT Server is
 │Trying To Restart...♻️
 │━━━━•🔰•━━━━┓
 │❖Restarting: in 6's
 │
 │
-│ ╔══╦╦╗─╔╦══╗
-│ ║╔╗║║╚╦╝╠║║╝
-│ ║╠╣║╚╗║╔╬║║╗
-│ ╚╝╚╩═╩═╝╚══╝
 │	     Restarted..
 │			 🄱🄾🅃
-│  Bot by ALVI
+│  Bot by SUJON BE'VIES
 │━━━━•♻️•━━━━┛
-│𝗠𝗥.𝗔𝗟𝗩𝗜 𝗦𝗘𝗥𝗩𝗘𝗥 is
+│SARA-ROBOT Server is
 │Successfully Restarted
 ╰──────────────➣`, threadID, () => process.exit(1));
 }
